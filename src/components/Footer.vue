da6a07942e6220b6019018bc7518e86c1c76f8bb
nobita <nobita@tanyu.mobi>
2024-09-27 19:11:53 +0800

    update🆙: 更新备案

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -1,6 +1,13 @@
 <template>
-    <div class="mb-6 prose m-auto flex slide-enter animate-delay-1200!">
-        <span class="text-sm op50"><a target="_blank" href="https://creativecommons.org/licenses/by-nc-sa/4.0/" style="color:inherit">CC BY-NC-SA 4.0</a> 2021-PRESENT © Nobita Yuan</span>
-        <div class="flex-auto" />
+    <div class="mt-10 mb-6 prose m-auto flex slide-enter animate-delay-1200! ">
+        <div class="text-sm op50">
+            <a href="https://creativecommons.org/licenses/by-nc-sa/4.0/" target="_blank" style="color:inherit">
+                CC BY-NC-SA 4.0
+            </a>
+            2021-PRESENT © Nobita Yuan
+            <!-- <br> -->
+             &nbsp;
+            <a href="https://beian.miit.gov.cn/" target="_blank" style="color:inherit">湘ICP备2024086022号</a>
+        </div>
     </div>
 </template>
